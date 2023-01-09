6f1c6c071ac1422d78e914267643e58df49a7970
Troy Budd <30150423+TroyB95@users.noreply.github.com>
2023-01-09 10:13:01 +0000

    TB - Update readme, use semantic html

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,9 +27,16 @@ const StyledMainContentArea = styled.main`
   }
 `;
 
+const StyledArticle = styled.article`
+  width: 70%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const StyledP = styled.p`
   margin-top: 2rem;
-  width: 70%;
 
   @media (min-width: 1024px) {
     width: 50%;
@@ -48,43 +55,47 @@ const Home = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <StyledMainContentArea>
-      <h1>Troy Budd</h1>
-      <StyledP>
-        Forward-thinking Software Engineer & Mentor with a background working
-        productively in dynamic environments. Fluent in TypeScript and React and
-        have found a real passion for being part of the full product lifecycle.
-        Proud team player focused on achieving project objectives with speed and
-        accuracy.
-        <br />
-        <br />I am currently at{" "}
+      <header>
+        <h1>Troy Budd</h1>
+      </header>
+      <StyledArticle>
+        <StyledP>
+          Forward-thinking Software Engineer & Mentor with a background working
+          productively in dynamic environments. Fluent in TypeScript and React
+          and have found a real passion for being part of the full product
+          lifecycle. Proud team player focused on achieving project objectives
+          with speed and accuracy.
+          <br />
+          <br />I am currently at{" "}
+          <a
+            href="https://www.infogrid.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            InfoGrid
+          </a>{" "}
+          working on the Occupancy team delivering smart solutions based around
+          building Occupancy. I am also a Mentor for{" "}
+          <a
+            href="https://academy.tech/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            academy.tech
+          </a>{" "}
+          mentoring early stage juniors who have just been placed into new roles
+          <br />
+          <br />
+          Based in London, I enjoy all things Climbing, Tech, Adventure.
+        </StyledP>
         <a
-          href="https://www.infogrid.io/"
+          href="https://www.linkedin.com/in/troybudd"
           target="_blank"
           rel="noopener noreferrer"
         >
-          InfoGrid
-        </a>{" "}
-        working on the Occupancy team delivering smart solutions based around
-        building Occupancy. I am also a Mentor for{" "}
-        <a
-          href="https://academy.tech/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          academy.tech
-        </a>{" "}
-        mentoring early stage juniors who have just been placed into new roles
-        <br />
-        <br />
-        Based in London, I enjoy all things Climbing, Tech, Adventure.
-      </StyledP>
-      <a
-        href="https://www.linkedin.com/in/troybudd"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <LinkedinLogo />
-      </a>
+          <LinkedinLogo />
+        </a>
+      </StyledArticle>
     </StyledMainContentArea>
   </>
 );
